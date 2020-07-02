309c4b90bfff13d859bd0b9dffe4929f877f256c
actondev <chris.actondev@gmail.com>
2020-07-02 13:59:28 +0200

    added imgui/m-begin2 with define-macro* as reference

--- src/scheme/imgui_macros.scm
+++ src/scheme/imgui_macros.scm
@@ -5,6 +5,17 @@
      ,@body
      (imgui/end)))
 
+(define-macro* (imgui/m-begin2 (title "") (*open #t) :rest body)
+  (if (eq? #t *open)
+      `(begin
+	 (imgui/begin ,title)
+	 ,@body
+	 (imgui/end))
+      `(begin
+	 (imgui/begin ,title ,*open)
+	 ,@body
+	 (imgui/end))))
+
 (comment "examples"
  (macroexpand (imgui/m-begin ("title")
 			     (imgui/text "hi")
@@ -20,6 +31,22 @@
 			     ))
  ;; =>
  (begin (imgui/begin "test" 'the-c-object) (imgui/text "hi") (imgui/text "scheme s7") (imgui/end))
+
+
+ (macroexpand (imgui/m-begin2 :title "always open"
+			     (imgui/text "hi")
+			     (imgui/text "scheme s7")
+			     ))
+ ;; =>
+ (begin (imgui/begin "always open" (imgui/text "hi")) (imgui/text "scheme s7") (imgui/end))
+
+ (macroexpand (imgui/m-begin2 :title "always open"
+			      :*open 'the-c-object
+			     (imgui/text "hi")
+			     (imgui/text "scheme s7")
+			     ))
+ ;; =>
+ (begin (imgui/begin "always open" 'the-c-object) (imgui/text "hi") (imgui/text "scheme s7") (imgui/end))
  
  )
 
