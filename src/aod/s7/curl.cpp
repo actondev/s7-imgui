92652c7e22dadf621e5d3d31e3eba21facd279fa
actondev <chris.actondev@gmail.com>
2020-09-15 03:39:31 +0200

    curl with make_function_star (define* (curl file (out #f) (opts ..))

--- src/aod/s7/curl.cpp
+++ src/aod/s7/curl.cpp
@@ -7,57 +7,115 @@ namespace aod {
 namespace s7 {
 namespace curl {
 
-size_t write_data_stringstream(void *ptr, size_t size, size_t nmemb, void *stream) {
-    std::string data((const char*) ptr, (size_t) size * nmemb);
-    // Note: https://techoverflow.net/2013/03/15/simple-c-http-download-using-libcurl-easy-api/
-    // the snippet there was streaming std::endl after the data... why??
+// https://github.com/isocpp/CppCoreGuidelines/blob/master/CppCoreGuidelines.md
+template<typename A>
+struct final_action {   // slightly simplified
+    A act;
+    final_action(A a) : act{a} {}
+    ~final_action() {
+        act();
+    }
+};
 
-    *((std::stringstream*) stream) << data;
-    return size * nmemb;
+template<typename A>
+final_action<A> finally(A act) { // deduce action type
+    return final_action<A> {act};
 }
+
+/**
+ * The following were helpful:
+ * - https://techoverflow.net/2013/03/15/simple-c-http-download-using-libcurl-easy-api/
+ * - https://gist.github.com/alghanmi/c5d7b761b2c9ab199157
+ */
 
 size_t write_data_string(void *contents, size_t size, size_t nmemb, void *userp) {
     ((std::string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
 }
 
-// TODO that's a nice case for a lambda* actually
-// passing named options
-// eg :ssl-verify-peer 0
-// eg :out "some-file.html"
-const char* help_curl = "(curl url)";
-s7_pointer curl(s7_scheme* sc, s7_pointer args) {
+static size_t write_data_file(void *ptr, size_t size, size_t nmemb, void *stream) {
+    size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
+    return written;
+}
+
+const char* curl_default_opts = R"__(
+(define *default-curl-opts*
+ (inlet
+   'ssl-verify-peer 1
+   ;; 'no-signal 1
+   'follow-location 1
+   ))
+;;)__";
+const char* help_curl_star = "(curl url (out #f) (opts *default-curl-opts*))";
+const char* args_curl_star = "url (out #f) (opts (inlet))";
+s7_pointer curl_star(s7_scheme* sc, s7_pointer args) {
+
     const char* url = s7_string(s7_car(args));
 
-    // https://techoverflow.net/2013/03/15/simple-c-http-download-using-libcurl-easy-api/
-    // https://gist.github.com/alghanmi/c5d7b761b2c9ab199157
+    // out-file
+    args = s7_cdr(args);
+    s7_pointer pfile = s7_car(args);
+
+    // curl-opts
+    args = s7_cdr(args);
+    s7_pointer passed_opts = s7_car(args);
+
+    s7_pointer default_opts = s7_eval_c_string(sc, "(aod.c.curl '*default-curl-opts*)");
+    s7_pointer undefined = s7_undefined(sc);
+    auto get_opt = [=](const char* opt_name)->int{
+        s7_pointer symbol = s7_make_symbol(sc, opt_name);
+        s7_pointer p = s7_let_ref(sc, passed_opts, symbol);
+        if (p == undefined) {
+            p = s7_let_ref(sc, default_opts, symbol);
+        }
+        return s7_integer(p);
+    };
+
     CURL* curl = curl_easy_init();
+    auto __curl = finally([curl] { curl_easy_cleanup(curl); });
+
     curl_easy_setopt(curl, CURLOPT_URL, url);
-    /* example.com is redirected, so we tell libcurl to follow redirection */
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
     curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1); //Prevent "longjmp causes uninitialized stack frame" bug
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, get_opt("follow-location"));
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, get_opt("ssl-verify-peer"));
 
-    // * SSL certificate problem: self signed certificate in certificate chain
-    // TODO what should I do with this???
-    // this happens ALWAYS
-    // maybe I miss some configuration??
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    if (s7_is_string(pfile)) {
+        FILE *outfile = fopen(s7_string(pfile), "wb");
+        if (outfile) {
+            // NOTE: even without the writefunction set, it WAS writting to outfile!
+            // but it was slower I think
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data_file);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, outfile);
 
-    std::string out;
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data_string);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &out);
-
-    CURLcode res = curl_easy_perform(curl);
-    /* Check for errors */
-    if (res != CURLE_OK) {
-        // todo s7 error
-        fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                curl_easy_strerror(res));
+            /* get it! */
+            CURLcode res = curl_easy_perform(curl);
+            if (res != CURLE_OK) {
+                // todo s7 error
+                fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                        curl_easy_strerror(res));
+            }
+            /* close the header file */
+            fclose(outfile);
+            return s7_make_boolean(sc, res == CURLE_OK);
+        } else {
+            fprintf(stderr, "Could not open file %s\n", s7_string(pfile));
+            return s7_f(sc);
+        }
+    } else {
+        // writing to a string and returning it
+        std::string out;
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data_string);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &out);
+        CURLcode res = curl_easy_perform(curl);
+        /* Check for errors */
+        if (res != CURLE_OK) {
+            // todo s7 error
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+            return s7_f(sc);
+        }
+        return s7_make_string(sc, out.c_str());
     }
-
-
-    curl_easy_cleanup(curl);
-    return s7_make_string(sc, out.c_str());
 }
 
 void bind(s7_scheme* sc) {
@@ -67,11 +125,12 @@ void bind(s7_scheme* sc) {
     s7_pointer env = s7_inlet(sc, s7_nil(sc));
     s7_gc_protect(sc, env);
 
-    s7_define(sc, env, s7_make_symbol(sc, "curl"),
-              s7_make_function(sc, "curl", curl,
-                               1, 0, false,
-                               help_curl));
+    s7_eval_c_string_with_environment(sc,
+                                      curl_default_opts, env);
 
+    s7_define(sc, env, s7_make_symbol(sc, "curl"),
+              s7_make_function_star(sc, "curl", curl_star,
+                                    args_curl_star, help_curl_star));
 
     s7_define_variable(sc, "aod.c.curl", env);
 }
--- test/aod/s7/curl_test.cpp
+++ test/aod/s7/curl_test.cpp
@@ -1,14 +1,25 @@
 #include <gtest/gtest.h>
 #include "s7.h"
 #include "aod/s7/curl.hpp"
+#include "aod/s7/json.hpp"
 
 TEST(curl, get) {
     s7_scheme* sc = s7_init();
     aod::s7::curl::bind(sc);
+    aod::s7::json::bind(sc);
     
     s7_eval_c_string(sc, "(define curl (aod.c.curl 'curl))");
+    s7_eval_c_string(sc, "(define parse (aod.c.json 'parse))");
     
-    s7_eval_c_string(sc, "(display (curl \"https://httpbin.org/get\"))");
+    s7_eval_c_string(sc, "(define res (curl \"https://httpbin.org/get?foo=bar\" :opts (inlet 'ssl-verify-peer 0)))");
+    s7_eval_c_string(sc, "(define parsed (parse res))");
+    s7_pointer foo = s7_eval_c_string(sc, "(define foo (parsed \"args\" \"foo\"))");
     
-    s7_free(sc);
+    ASSERT_STREQ("bar", s7_string(foo));
+    // TODO remove possible httpbin.json file
+    s7_eval_c_string(sc, "(define res2 (curl \"https://httpbin.org/get?foo=bar\" :out \"httpbin.json\" :opts (inlet 'ssl-verify-peer 0)))");
+    // TODO test that file httpbin.json exists, open, parse with json again
+    
+    // TODO s7_free crashes with star function definition
+//     s7_free(sc);
 }
